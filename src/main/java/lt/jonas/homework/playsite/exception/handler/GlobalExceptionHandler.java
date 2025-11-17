3c9e85e5e878b54385ee2c4dfda5bc208d3f4b46
Jonas Butvilas <butv@danskebank.lt>
2025-11-17 12:40:18 +0200

    Add docs where required

--- src/main/java/lt/jonas/homework/playsite/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/lt/jonas/homework/playsite/exception/handler/GlobalExceptionHandler.java
@@ -12,6 +12,10 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+/**
+ * Global exception handler for handling exceptions across the application.
+ * It provides consistent error response interface for different types of exceptions.
+ */
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -21,47 +25,80 @@ public class GlobalExceptionHandler {
     private static final String METHOD_NOT_SUPPORTED = "METHOD_NOT_SUPPORTED";
     private static final String INVALID_REQUEST = "INVALID_REQUEST";
 
+    /**
+     * Handles {@link TicketUsedException}.
+     *
+     * @return a response entity with the error response and HTTP status 400 (Bad Request)
+     */
     @ExceptionHandler(TicketUsedException.class)
     public ResponseEntity<ErrorResponseDto> handleTicketUsedException(TicketUsedException ex) {
         ErrorResponseDto errorResponse = new ErrorResponseDto(ex.getMessage(), TICKET_ALREADY_USED);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
+    /**
+     * Handles {@link ResourceNotFoundException}.
+     *
+     * @return a response entity with the error response and HTTP status 404 (Not Found)
+     */
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorResponseDto> handlePlaysiteOutOfCapacityException(ResourceNotFoundException ex) {
+    public ResponseEntity<ErrorResponseDto> handleResourceNotFoundException(ResourceNotFoundException ex) {
         ErrorResponseDto errorResponse = new ErrorResponseDto(ex.getMessage(), RESOURCE_NOT_FOUND);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 
+    /**
+     * Handles {@link PlaySiteOutOfCapacityException}.
+     *
+     * @return a response entity with the error response and HTTP status 400 (Bad Request)
+     */
     @ExceptionHandler(PlaySiteOutOfCapacityException.class)
     public ResponseEntity<ErrorResponseDto> handlePlaysiteOutOfCapacityException(PlaySiteOutOfCapacityException ex) {
         ErrorResponseDto errorResponse = new ErrorResponseDto(ex.getMessage(), PLAYSITE_OUT_OF_CAPACITY);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
+    /**
+     * Handles {@link MethodArgumentNotValidException}.
+     *
+     * @return a response entity with the error response and HTTP status 400 (Bad Request)
+     */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponseDto> handleValidationExceptions(MethodArgumentNotValidException ex) {
         StringBuilder errorMessage = new StringBuilder();
         ex.getBindingResult().getFieldErrors().forEach(error -> errorMessage.append(error.getField())
-                .append(": ")
-                .append(error.getDefaultMessage())
-                .append("; "));
+                .append(": ").append(error.getDefaultMessage()).append("; "));
         ErrorResponseDto errorResponse = new ErrorResponseDto(errorMessage.toString(), INVALID_REQUEST);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
+    /**
+     * Handles {@link HttpRequestMethodNotSupportedException}.
+     *
+     * @return a response entity with the error response and HTTP status 405 (Method Not Allowed)
+     */
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ResponseEntity<ErrorResponseDto> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException ex) {
         ErrorResponseDto errorResponse = new ErrorResponseDto(ex.getMessage(), METHOD_NOT_SUPPORTED);
         return new ResponseEntity<>(errorResponse, HttpStatus.METHOD_NOT_ALLOWED);
     }
 
+    /**
+     * Handles {@link HttpMessageConversionException}.
+     *
+     * @return a response entity with the error response and HTTP status 400 (Bad Request)
+     */
     @ExceptionHandler(HttpMessageConversionException.class)
-    public ResponseEntity<ErrorResponseDto> handleHttpRequestMethodNotSupportedException(HttpMessageConversionException ex) {
+    public ResponseEntity<ErrorResponseDto> handleHttpMessageConversionException(HttpMessageConversionException ex) {
         ErrorResponseDto errorResponse = new ErrorResponseDto(ex.getMessage(), INVALID_REQUEST);
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
+    /**
+     * Handles all unexpected exceptions that are not explicitly handled.
+     *
+     * @return a response entity with the error response and HTTP status 500 (Internal Server Error)
+     */
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponseDto> handleGlobalExceptions() {
         ErrorResponseDto errorResponse = new ErrorResponseDto("An unexpected error occurred", "INTERNAL_SERVER_ERROR");
--- src/main/java/lt/jonas/homework/playsite/service/PlaySiteService.java
+++ src/main/java/lt/jonas/homework/playsite/service/PlaySiteService.java
@@ -1,5 +1,6 @@
 package lt.jonas.homework.playsite.service;
 
+import lombok.RequiredArgsConstructor;
 import lt.jonas.homework.playsite.exception.model.PlaySiteOutOfCapacityException;
 import lt.jonas.homework.playsite.exception.model.ResourceNotFoundException;
 import lt.jonas.homework.playsite.exception.model.TicketUsedException;
@@ -18,25 +19,41 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Service class for managing play sites and their associated resources.
+ */
 @Service
+@RequiredArgsConstructor
 public class PlaySiteService {
 
     private final PlaySiteRepository playSiteRepository;
     private final KidRepository kidRepository;
 
-    public PlaySiteService(PlaySiteRepository playSiteRepository, KidRepository kidRepository) {
-        this.playSiteRepository = playSiteRepository;
-        this.kidRepository = kidRepository;
-    }
-
+    /**
+     * Retrieves all play sites.
+     *
+     * @return a list of all play sites
+     */
     public List<PlaySite> getAllPlaySites() {
         return playSiteRepository.findAll();
     }
 
+    /**
+     * Retrieves a play site by its ID.
+     *
+     * @param id the ID of the play site
+     * @return an optional containing the play site if found
+     */
     public Optional<PlaySite> getPlaySiteById(Long id) {
         return playSiteRepository.findById(id);
     }
 
+    /**
+     * Creates a new play site based on the request.
+     *
+     * @param playSiteRequest the request containing play site details
+     * @return the created play site
+     */
     public PlaySite createPlaySite(PlaySiteRequest playSiteRequest) {
         PlaySite playSite = new PlaySite();
         playSite.setName(playSiteRequest.name());
@@ -44,6 +61,14 @@ public class PlaySiteService {
         return playSiteRepository.save(playSite);
     }
 
+    /**
+     * Updates an existing play site by its ID and moves kids from queue to play site
+     * if capacity is increased.
+     *
+     * @param id the ID of the play site
+     * @param playSiteRequest the request containing updated play site details
+     * @return the updated play site
+     */
     public PlaySite updatePlaySite(Long id, PlaySiteRequest playSiteRequest) {
         PlaySite playSite = getPlaySite(id);
         playSite.getAttractions().clear();
@@ -55,10 +80,23 @@ public class PlaySiteService {
         return playSiteRepository.save(playSite);
     }
 
+    /**
+     * Deletes a play site by its ID.
+     *
+     * @param id the ID of the play site
+     */
     public void deletePlaySite(Long id) {
         playSiteRepository.deleteById(id);
     }
 
+    /**
+     * Adds a kid to a play site. If the play site is full and queueing is accepted, the kid is added to the queue.
+     *
+     * @param playSiteId the ID of the play site
+     * @param kidRequest the request containing kid details
+     * @param acceptQueue whether to add the kid to the queue if the play site is full
+     * @return the added kid
+     */
     public Kid addKidToPlaySite(Long playSiteId, KidRequest kidRequest, boolean acceptQueue) {
         var playSite = getPlaySite(playSiteId);
         if (kidRepository.existsById(kidRequest.ticketNumber())) {
@@ -68,17 +106,22 @@ public class PlaySiteService {
         var kid = KidsMapper.toEntity(kidRequest);
         if (playSite.addKid(kid)) {
             playSiteRepository.save(playSite);
-        } else if (acceptQueue){
+        } else if (acceptQueue) {
             playSite.getQueue().add(kid.getTicketNumber());
             kidRepository.save(kid);
         } else {
             var message = "Playsite is at full capacity of %s kids and cannot accept any more at the moment".formatted(playSite.getTotalCapacity());
             throw new PlaySiteOutOfCapacityException(message);
         }
-
         return kid;
     }
 
+    /**
+     * Removes a kid from the play site or its queue.
+     *
+     * @param playSiteId the ID of the play site
+     * @param ticketNumber the ticket number of the kid
+     */
     public void removeKidFromPlaySite(Long playSiteId, Long ticketNumber) {
         var playSite = getPlaySite(playSiteId);
         var kid = getKid(ticketNumber);
@@ -95,18 +138,34 @@ public class PlaySiteService {
         playSiteRepository.save(playSite);
     }
 
+    /**
+     * Retrieves the total visitor count for all play sites for today.
+     *
+     * @return a DTO containing the total visitor count
+     */
     public TotalVisitorsResponseDto getTotalVisitorCountForToday() {
         var playSites = playSiteRepository.findAll();
         var totalVisitorCount = playSites.stream().mapToInt(PlaySite::getDailyVisitorCount).sum();
         return new TotalVisitorsResponseDto(playSites.size(), totalVisitorCount);
     }
 
+    /**
+     * Retrieves the utilisation percentage of a play site.
+     *
+     * @param playSiteId the ID of the play site
+     * @return a response dto containing the utilisation percentage
+     */
     public UtilisationResponseDto getPlaySiteUtilisation(Long playSiteId) {
         PlaySite playSite = getPlaySite(playSiteId);
         double utilisation = playSite.getTotalCapacity() == 0 ? 0 : (double) playSite.getKids().size() / playSite.getTotalCapacity() * 100;
         return new UtilisationResponseDto(playSiteId, utilisation);
     }
 
+    /**
+     * Attempts to add kids from the queue to the play site if there is available capacity.
+     *
+     * @param playSite the play site
+     */
     private void tryAddingKidsFromQueue(PlaySite playSite) {
         var newCapacity = playSite.getTotalCapacity();
         var currentKidCount = playSite.getKids().size();
@@ -126,10 +185,22 @@ public class PlaySiteService {
         }
     }
 
+    /**
+     * Retrieves the next kid in the queue for a play site.
+     *
+     * @param playSite the play site
+     * @return an optional containing the next kid in the queue, if present
+     */
     private Optional<Kid> getNextKidInQueue(PlaySite playSite) {
         return dequeueNext(playSite).map(this::getKid);
     }
 
+    /**
+     * Dequeues the next ticket number from the play site's queue.
+     *
+     * @param playSite the play site
+     * @return an optional containing the next ticket number, if present
+     */
     private Optional<Long> dequeueNext(PlaySite playSite) {
         var queue = playSite.getQueue();
         if (!queue.isEmpty()) {
@@ -140,11 +211,25 @@ public class PlaySiteService {
         return Optional.empty();
     }
 
+    /**
+     * Retrieves a play site by its ID or throws an exception if not found.
+     *
+     * @param playSiteId the ID of the play site
+     * @return the play site
+     * @throws ResourceNotFoundException if the play site is not found
+     */
     private PlaySite getPlaySite(Long playSiteId) {
         return playSiteRepository.findById(playSiteId)
                 .orElseThrow(() -> new ResourceNotFoundException("Playsite by id: %s not found".formatted(playSiteId)));
     }
 
+    /**
+     * Retrieves a kid by their ticket number or throws an exception if not found.
+     *
+     * @param ticketNumber the ticket number of the kid
+     * @return the kid
+     * @throws ResourceNotFoundException if the kid is not found
+     */
     private Kid getKid(Long ticketNumber) {
         return kidRepository.findById(ticketNumber)
                 .orElseThrow(() -> new ResourceNotFoundException("Kid by ticketNumber: %s not found".formatted(ticketNumber)));
